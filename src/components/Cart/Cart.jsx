0cdf3e4b808ae82039ff50dac1de31735480a0f3
Martin Jusbicher <jusbichermartin@gmail.com>
2022-05-18 15:36:59 -0300

    deploy

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,18 @@
-import Button from '@mui/material/Button';
 import React, { useContext } from "react";
+import Button from '@mui/material/Button';
 import { CartContext } from "../../Context/CartContext";
 import styles from "./Cart.module.css"
 import { Link } from 'react-router-dom'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+      neutral: {
+      main: '#111',
+      contrastText: '#fff',
+      },
+    },
+});
 
 const Cart = () => {
   const { cart, removeItem, clear, totalPrice } = useContext(CartContext);
@@ -11,11 +21,13 @@ const Cart = () => {
     <>
         <div className={styles.header}>
           <h2 className={styles.heading}>Lista de Productos: {cart.length}</h2>
-          <h5 className={styles.action} onClick={() => clear()}>Limpiar Carrito</h5>
+          <h5 className={styles.action} onClick={() => clear()}>LIMPIAR CARRITO </h5>
         </div>
       <div>{cart.length === 0 && <div>
-        <h2 className={styles.texts}>El carrito está vacío</h2>
-        <Link to="/" className={styles.texts}>Volver a la página principal</Link>
+        <h2 className={styles.texts}>EL CARRITO ESTÁ VACÍO</h2>
+        <Link to="/" className={styles.texts}>
+          <Button variant="contained">Volver a la página principal</Button>
+        </Link>
       </div>}</div>
       {cart.length > 0 &&
         cart.map((p) => (
@@ -33,11 +45,15 @@ const Cart = () => {
                 <Button size="small" variant="contained" color="success" className={styles.btn}>+</Button>
               </div>
             </div>
-            <Button onClick={() => removeItem(p.id)}>Remover</Button>
+            <Button onClick={() => removeItem(p.id)}>REMOVER</Button>
           </div>
         ))}
-        <h2 className={styles.texts}>Valor total: &#36;{totalPrice}</h2>
-        <Link to="/checkout" className={styles.texts}>FINALIZAR COMPRA</Link>
+        <h2 className={styles.texts}>VALOR TOTAL: &#36;{totalPrice}</h2>
+        <Link to="/checkout" className={styles.texts}>
+          <ThemeProvider theme={theme}>
+            <Button variant="contained" color="neutral">FINALIZAR COMPRA</Button>
+          </ThemeProvider>
+        </Link>
     </>
   );
 };
